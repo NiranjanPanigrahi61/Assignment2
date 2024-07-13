3e6b6f9522bc7b5cbf0d4c6c8671804dd7efd2fe
NiranjanPanigrahi61 <156101527+NiranjanPanigrahi61@users.noreply.github.com>
2024-07-13 08:15:43 +0530

    adding js to cancle button.

--- js/active.js
+++ js/active.js
@@ -4,43 +4,57 @@ function activeHome(){
     document.getElementById("departmentOption").classList.remove("active");
     document.getElementById("mediaOption").classList.remove("active");
     document.getElementById("myAccounntOption").classList.remove("active");
-
 }
 function activeCustomer(){
     document.getElementById("homeOption").classList.remove("active");
     document.getElementById("customerOption").classList.add("active");
     document.getElementById("departmentOption").classList.remove("active");
     document.getElementById("mediaOption").classList.remove("active");
-    document.getElementById("myAccounntOption").classList.remove("active");
-    
+    document.getElementById("myAccounntOption").classList.remove("active");   
 }
 function activeDepartment(){
     document.getElementById("homeOption").classList.remove("active");
     document.getElementById("customerOption").classList.remove("active");
     document.getElementById("departmentOption").classList.add("active");
     document.getElementById("mediaOption").classList.remove("active");
-    document.getElementById("myAccounntOption").classList.remove("active");
-    
+    document.getElementById("myAccounntOption").classList.remove("active");    
 }
 function activeMedia(){
     document.getElementById("homeOption").classList.remove("active");
     document.getElementById("customerOption").classList.remove("active");
     document.getElementById("departmentOption").classList.remove("active");
     document.getElementById("mediaOption").classList.add("active");
-    document.getElementById("myAccounntOption").classList.remove("active");
-    
+    document.getElementById("myAccounntOption").classList.remove("active");   
 }
 function activeMyAccount(){
     document.getElementById("homeOption").classList.remove("active");
     document.getElementById("customerOption").classList.remove("active");
     document.getElementById("departmentOption").classList.remove("active");
     document.getElementById("mediaOption").classList.remove("active");
-    document.getElementById("myAccounntOption").classList.add("active");
-    
+    document.getElementById("myAccounntOption").classList.add("active");   
+}
+function resetForm(){
+    document.getElementById("userName").removeAttribute("readonly");
+    document.getElementById("userEmail").removeAttribute("readonly");
+    document.getElementById("phnNumber").removeAttribute("readonly");
+    document.getElementById("dob").removeAttribute("readonly");
+
+    document.getElementById("userName").value = "John Doe";
+    document.getElementById("userEmail").value = "example@gmail.com";
+    document.getElementById("phnNumber").value = "9134837948";
+    document.getElementById("dob").value = "2023-05-09";
+
+    document.getElementById("userName").setAttribute('readonly',true);
+    document.getElementById("userEmail").setAttribute('readonly',true);
+    document.getElementById("phnNumber").setAttribute('readonly',true);
+    document.getElementById("dob").setAttribute('readonly',true);
 }
 function saveData(){
-    // window.alert("Data Updated.");
     document.getElementById("responseLabel").textContent=`Data is Updated`;
+    document.getElementById("userName").setAttribute('readonly',true);
+    document.getElementById("userEmail").setAttribute('readonly',true);
+    document.getElementById("phnNumber").setAttribute('readonly',true);
+    document.getElementById("dob").setAttribute('readonly',true);
 }
 function editField1(){
     document.getElementById("userName").removeAttribute('readonly');
